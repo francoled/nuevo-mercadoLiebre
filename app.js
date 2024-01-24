793de5a92c869932f2b39ce0e8c12597ec76de45
francoled <ledesmaf659@gmail.com>
2024-01-24 03:47:16 -0300

    nuevo mercado

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const path=require('path');
+const app=express();
+
+app.get('/register',function(req,res){
+res.sendFile(path.join(__dirname,'./views/register.html'))
+});
+app.get('/login',function(req,res){
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+    });
+
+const publicpath=path.resolve(__dirname,'./public')
+app.use(express.static('public'));
+const port=process.env.PORT || 3000;
+
+app.listen(port,() => {
+
+    console.log("servidor corriendo en el puerto"+ port)
+})
+app.get('/',(req, res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
